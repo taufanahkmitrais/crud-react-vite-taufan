763990f98d8d506b4a505b99912d1f49c39c9d86
taufanahkmitrais <Taufan_H390@mitrais.com>
2023-11-22 16:00:35 +0700

    add_crud_with_mock_api

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,116 @@
-import { QliphortScout, QliphortDisk } from "./TaufanExclusivePage";
-import Sharingan from "./TaufanExclusivePage";
-import { TodoList } from "./TodoList";
-import {Zoro} from "./monkey_pages/MonkeyDeLuffy";
-function App() {
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import axios from 'axios';
+import {
+  Button,
+  Input,
+  VStack,
+  Checkbox,
+  HStack,
+  Container,
+  Heading,
+  Center
+} from '@chakra-ui/react';
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  useEffect(() => {
+    const fetchTodos = async (): Promise<void> => {
+      const response = await axios.get<Todo[]>('http://localhost:5000/todos');
+      setTodos(response.data);
+    };
+
+    fetchTodos();
+  }, []);
+
+  const addTodo = async (): Promise<void> => {
+    const response = await axios.post<Todo>('http://localhost:5000/todos', {
+      title: newTodo,
+      completed: false
+    });
+
+    setTodos([...todos, response.data]);
+    setNewTodo('');
+  };
+
+  const updateTodo = async (id: number, title: string, completed: boolean): Promise<void> => {
+    console.log(title);
+    const updatedTodo = { id: id, title: title, completed: completed };
+    await axios.put(`http://localhost:5000/todos/${id}`, updatedTodo);
+    setTodos(todos.map(todo => (todo.id === id ? updatedTodo : todo)));
+  };
+
+  const deleteTodo = async (id: number): Promise<void> => {
+    await axios.delete(`http://localhost:5000/todos/${id}`);
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const toggleComplete = async (id: number): Promise<void> => {
+    const todo = todos.find(todo => todo.id === id);
+
+    if (todo) {
+      const updatedTodo = { ...todo, completed: !todo.completed };
+      await axios.put(`http://localhost:5000/todos/${id}`, updatedTodo);
+      setTodos(todos.map(todo => (todo.id === id ? updatedTodo : todo)));
+    }
+  };
 
   return (
-    <div className="App">
-      <Zoro />
-      <QliphortScout />
-      <QliphortDisk />
-      <Sharingan />
-      <TodoList />
-    </div>
-  )
-}
+    <Container maxW="container.md" p={5}>
+      <Center>
+        <Heading mb={5}>Simple Todo App</Heading>
+      </Center>
 
-export default App
+      <Input
+        value={ newTodo }
+        onChange={
+          (e) => setNewTodo(e.target.value)
+        }
+        placeholder="Add a new todo"
+      />
+
+      <Center mt={2} mb={5}>
+        <Button
+          onClick={ addTodo }
+          mt={2}
+        >Add Todo</Button>
+      </Center>
+
+      <VStack spacing={ 4 }>
+        { todos.map(todo => (
+          <HStack key={ todo.id }>
+            <Checkbox
+              isChecked={ todo.completed }
+              onChange={
+                () => toggleComplete(todo.id)
+              }
+            />
+            <Input
+              defaultValue={ todo.title }
+              onBlur={
+                (e) => updateTodo(todo.id, e.target.value, todo.completed)
+              }
+            />
+            <Button onClick={
+              () => deleteTodo(todo.id)
+            }>
+              Delete
+            </Button>
+          </HStack>
+        ))}
+      </VStack>
+    </Container>
+  );
+};
+
+export default App;
